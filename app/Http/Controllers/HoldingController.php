0d8cd72ce2ef70e6463f19f38d071856ac008043
biplabuddin99 <biplabuddin99@github.com>
2023-01-04 21:55:51 +0600

    পেশা বা আয়ের উৎস porjonto create done

--- app/Http/Controllers/HoldingController.php
+++ app/Http/Controllers/HoldingController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Holding;
 use Illuminate\Http\Request;
 use PhpParser\Node\Stmt\Return_;
+use Brian2694\Toastr\Facades\Toastr;
+use Exception;
 
 class HoldingController extends Controller
 {
@@ -36,7 +38,59 @@ class HoldingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try{
+            $holding=new Holding;
+            $holding->form_no=$request->form_no;
+            $holding->holding_date=$request->holding_date;
+            $holding->head_household=$request->head_household;
+            $holding->father_name=$request->father_name;
+            $holding->mother_name=$request->mother_name;
+            $holding->husband_wife=$request->husband_wife;
+            $holding->new_holding_no=$request->new_holding_no;
+            $holding->previous_holding_no=$request->previous_holding_no;
+            $holding->village=$request->village;
+            $holding->ward_no=$request->ward_no;
+            $holding->birth_date=$request->birth_date;
+            $holding->voter_id_no=$request->voter_id_no;
+            $holding->phone=$request->phone;
+            $holding->email=$request->email;
+            $holding->marital_status=$request->marital_status;
+            $holding->gender=$request->gender;
+            $holding->digital_birth_cer=$request->digital_birth_cer;
+            $holding->paved_bathroom=$request->paved_bathroom;
+            $holding->expatriate=$request->expatriate;
+            $holding->power_connection=$request->power_connection;
+            $holding->tube_well=$request->tube_well;
+            $holding->bank=$request->bank;
+            $holding->edu_qual=implode(',',$request->edu_qual);
+            $holding->family_male=$request->family_male;
+            $holding->family_female=$request->family_female;
+            $holding->family_total=$request->family_total;
+            $holding->single_joint_family=$request->single_joint_family;
+            $holding->religion=$request->religion;
+            $holding->mobile_bank=implode(',',$request->mobile_bank);
+            $holding->government_facilities=implode(',',$request->government_facilities);
+            $holding->family_status=$request->family_status;
+            $holding->digital_devices=implode(',',$request->digital_devices);
+            $holding->telecommunications=implode(',',$request->telecommunications);
+            $holding->source_income=implode(',',$request->source_income);
+            $holding->business_taxes=$request->business_taxes;
+            $holding->head_household=$request->head_household;
+            $holding->head_household=$request->head_household;
+            $holding->head_household=$request->head_household;
+            $holding->head_household=$request->head_household;
+            $holding->head_household=$request->head_household;
+            $holding->head_household=$request->head_household;
+            $holding->head_household=$request->head_household;
+            $holding->save();
+            Toastr::success('holding Created Successfully!');
+            return redirect(route('holding.index'));
+            // dd($request);
+        }
+        catch (Exception $e){
+            return back()->withInput();
+
+        }
     }
 
     /**
