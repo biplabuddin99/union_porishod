99b72a3956cf39c0d469adc26a39524654181bd5
BIPLAB UDDIN <biplabuddin990@gmail.com>
2023-05-20 21:36:21 +0600

    family certificate update

--- app/Http/Controllers/FamilyCertificateController.php
+++ app/Http/Controllers/FamilyCertificateController.php
@@ -7,6 +7,7 @@ use App\Models\EducationalQualification;
 use App\Models\Profession;
 use App\Models\Settings\Location\District;
 use App\Models\Ward_no;
+use App\Models\FamilyMemberChild;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Http\Request;
@@ -79,6 +80,66 @@ class FamilyCertificateController extends Controller
         $ward=Ward_no::all();
         $districts=District::select('id','name','name_bn')->get();
         return view('familycertificate.create_page2',compact('family','ward','districts'));
+    }
+
+        public function FormPartSecondUpdate(Request $request, $encrypted_id)
+    {
+        try{
+            $family = FamilyCertificate::findOrFail(Crypt::decrypt($encrypted_id));
+            // আবেদনের অন্যান্য তথ্য
+            $family->name_head_family=$request->name_head_family;
+            $family->comments_permanent_union=$request->comments_permanent_union;
+            $family->relationship_applicant=$request->relationship_applicant;
+            $family->num_male=$request->num_male;
+            $family->num_female=$request->num_female;
+            $family->num_male_vot=$request->num_male_vot;
+            $family->num_female_vot=$request->num_female_vot;
+            $family->house_holding_number=$request->house_holding_number;
+            $family->street_nm=$request->street_nm;
+            $family->village_name=$request->village_name;
+            $family->ward_id=$request->ward_id;
+            $family->post_office=$request->post_office;
+            $family->union_id=$request->union_id;
+            $family->upazila_id=$request->upazila_id;
+            $family->district_id=$request->district_id;
+
+            if($request->has('image'))
+            $family->image=$this->resizeImage($request->image,'uploads/warishan',true,300,300,false);
+
+            if($request->has('nid_image'))
+            $family->nid_image=$this->resizeImage($request->nid_image,'uploads/warishan',true,500,500,false);
+
+            if($request->has('digital_birth_certificate'))
+            $family->digital_birth_certificate=$this->resizeImage($request->digital_birth_certificate,'uploads/warishan',true,500,700,false);
+            $family->status=1;
+            $family->chairman_id=request()->session()->get('upsetting')?request()->session()->get('upsetting')->chairman_id:"1";
+            if($family->save()){
+                if($request->cname){
+                    foreach($request->cname as $key => $value){
+                        // dd($request->all());
+                        if($value){
+                        $cwarisan = new FamilyMemberChild;
+                        $cwarisan->family_certificate_id=$family->id;
+                        $cwarisan->name=$request->cname[$key];
+                        $cwarisan->ralation=$request->crelation[$key];
+                        $cwarisan->birth_date=$request->cbirth_date[$key];
+                        $cwarisan->cnid=$request->cnid[$key];
+                        $cwarisan->ccomments=$request->ccomments[$key];
+                        $cwarisan->save();
+                    }
+                    }
+                }
+                Toastr::success('পরিবারের সনদ সফলভাবে তৈরি করা হয়েছে!!');
+                return redirect(route('warishan_primary.list',Crypt::encrypt($family->id)));
+            }else{
+                Toastr::success('দয়করে আবার চেষ্টা করুন!');
+                return redirect()->back();
+
+            }
+        }
+        catch (Exception $e){
+            return back()->withInput();
+        }
     }
 
     /**
